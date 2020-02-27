2da7d10090ca4ccf0ca5340c37c220833e056920
Albin <albinesteban@outlook.com>
2020-02-27 17:12:21 -0400

    Added function to find the primes that make up N

--- decrypt.py
+++ decrypt.py
@@ -22,14 +22,21 @@ def sieve_of_eratosthenes(limit):
     natural_numbers = [x for x in range(0, len(A) - 1) if A[x] is True]
     return natural_numbers
 
+def primes_of_n(num, primes):
+    for x in primes:
+        for y in primes:
+            if x * y == num:
+                return (x,y)
+    return -1
 
 binary_number = '00110100 00111001 00100000 00110001 00110000 00110111 00100000 00110100 00110110'
-e = 13
+E = 13
 N = 119
 list_of_numbers = binary_number.split(' ')
 numbers = decimal_to_text(list_of_numbers)
-res = sieve_of_eratosthenes(N)
-print(res)
+primes_less_than_n = sieve_of_eratosthenes(N)
+prime1, prime2 = primes_of_n(N, primes_less_than_n)
+print(prime1, prime2)
 # num1 = ''.join(numbers[0:2])
 # num2 = ''.join(numbers[2:5])
 # num3 = ''.join(numbers[5:])
