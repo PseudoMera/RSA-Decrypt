b06f524601dc87e27418c10d357ca7833c884927
Albin <albinesteban@outlook.com>
2020-02-27 18:44:17 -0400

    Final result to RASC decrypt

--- decrypt.py
+++ decrypt.py
@@ -1,6 +1,6 @@
 import math
 
-def decimal_to_text(encrypted_list):
+def binary_to_text(encrypted_list):
     numbers = []
     temp = ''
     for ch in encrypted_list:
@@ -49,6 +49,12 @@ def recipher_result(nums):
         results.append(pow(num, E, N))
     return results
 
+def decimal_to_chr(nums):
+    message = ''
+    for num in nums:
+        message = message + chr(num)
+    return message
+
 #Message to be decrypted
 binary_number = '00110100 00111001 00100000 00110001 00110000 00110111 00100000 00110100 00110110'
 E = 13
@@ -58,22 +64,32 @@ N = 119
 list_of_numbers = binary_number.split(' ')
 
 #Decrypted numbers
-numbers = decimal_to_text(list_of_numbers)
+numbers = binary_to_text(list_of_numbers)
+print("Decrypted numbers: ", numbers)
 
 #All primes from 2 to n
 primes_less_than_n = sieve_of_eratosthenes(N)
 
 #Two numbers that when multiplied equal N
 P, Q = primes_of_n(N, primes_less_than_n)
+print(f"Primes that compose N: {P} and {Q}")
 
 #(P - 1) * (Q - 1)
 PHI = (P - 1) * (Q - 1)
+print("PHI: ", PHI)
 
 #Key
 decryption_key = mod_inverse(E, PHI)
+print("Decryption key: ", decryption_key)
 
 #Results
 result = decrypt_result(numbers)
+print("Result message: ", result)
 
 #Recipher
 recipher_numbers = recipher_result(result)
+print("Recipher message: ", recipher_numbers)
+
+#Final Text
+message = decimal_to_chr(result)
+print("Final message: ", message)
